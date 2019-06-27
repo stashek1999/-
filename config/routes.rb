634c8e3f52a6de09260ef8d77e1520296e38873e
stashek1999 <stashek.jyra1999@gmail.com>
2019-06-27 22:23:19 +0300

    Add Bootstrap and add controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root 'pages#index'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :users, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
